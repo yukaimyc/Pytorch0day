24e33b3e9cbd4968dc0664f1f06adf1842b168de
yukaimyc <41511951+yukaimyc@users.noreply.github.com>
2020-01-12 14:47:16 +0800

    Add files via upload

---
+++ NEW way.py
@@ -0,0 +1,11 @@
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+net2=torch.nn.Sequential(
+    torch.nn.Linear(2,10),
+    torch.nn.ReLU(),
+    torch.nn.Linear(10,2),
+)
+print(net2)
